2f0bf0504665722a7ae14a6fd2a7b86a9507e5c6
Mafikul Islam <mdmafikulislam5320@gmail.com>
2026-05-01 16:05:38 +0600

    Backend setup

---
+++ src/app/app.ts
@@ -0,0 +1,18 @@
+import express, { Application } from 'express'
+import cors from 'cors'
+
+
+const app: Application = express()
+app.use(express.json())
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:5000",
+    credentials: true
+}))
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+export default app;
